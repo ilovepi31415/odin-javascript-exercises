3c0534c6bae9137bcc648ff43620fd46313da7f3
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-03 21:26:54 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    message = '';
+    for (i = 0; i < num; i++) {
+        message += str;
+    }
+    return message;
 };
 
 // Do not edit below this line
