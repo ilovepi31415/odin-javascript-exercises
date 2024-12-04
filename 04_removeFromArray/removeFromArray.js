8e83727377ceb6003ad2be3453fb2de8a5cdff1a
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-03 21:54:58 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    for (const item of itemsToRemove) {
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
