3d57f821090f6ee44f46b54aa235748af007ee0e
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-03 21:38:05 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+    let reveresedMessage = '';
+    const length = message.length;
+    for (i = 0; i < length; i++) {
+        reveresedMessage += message[length - 1 - i];
+    }
+    return reveresedMessage;
 };
 
 // Do not edit below this line
