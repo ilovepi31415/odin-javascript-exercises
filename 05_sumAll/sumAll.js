f185f57eeb37433c8e9dbd9ab3d68d18b4495276
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-06 12:14:03 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((typeof(num1) != 'number' || typeof(num2) != 'number') ||
+        (num1 % 1 != 0 || num2 % 1 != 0) || 
+        (num1 < 0 || num2 < 0)) {
+        return 'ERROR';
+    }
+    let total = 0;
+    let start, end;
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+    else {
+        start = num1;
+        end = num2;
+    }
+    for (i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
