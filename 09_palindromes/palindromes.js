0fe174878910896c336da54f9d8b0e99eeb7fd16
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-11 10:27:35 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let arr = [];
+    for (i = 0; i < s.length; i++) {
+        if (s[i].match(/[a-z0-9]/i)) {
+            arr.push(s[i].toLowerCase());
+        }
+    }
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] != arr[arr.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
