633aa73f71e1378502e4fa566b52063b9e584d65
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-11 11:01:30 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const ages = arr.map(findAge);
+    let oldest = 0;
+    let oldestIndex = -1;
+    for (i = 0; i < ages.length; i++) {
+        const age = ages[i];
+        if (age > oldest) {
+            oldest = age;
+            oldestIndex = i;
+        }
+    }
+    console.log(oldestIndex);
+    return arr[oldestIndex];
 };
+
+const findAge = function(person) {
+    const birth = person.yearOfBirth;
+    const death = ('yearOfDeath' in person) ? person.yearOfDeath : new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
